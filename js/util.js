5b8f04c76450a7d287bf497e4d622d6dfd9cf33c
Stephen Welch <stephenwelchva@gmail.com>
2019-05-07 20:48:15 -0400

    Migrate to NPM, switch to UL for navbar, WIP navbar move

---
+++ js/util.js
@@ -0,0 +1,36 @@
+/*!
+ * Check if an element is out of the viewport
+ * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Node}  elem The element to check
+ * @return {Object}     A set of booleans for each side of the element
+ */
+function isOutOfViewport(elem) {
+
+    // Get element's bounding
+    let bounding = elem.getBoundingClientRect();
+
+    // Check if it's out of the viewport on each side
+    let out = {};
+    out.top = bounding.top < 0;
+    out.left = bounding.left < 0;
+    out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
+    out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
+    out.any = out.top || out.left || out.bottom || out.right;
+    out.all = out.top && out.left && out.bottom && out.right;
+
+    return out;
+
+};
+
+function getPosition(element) {
+    var xPosition = 0;
+    var yPosition = 0;
+
+    while(element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return { x: xPosition, y: yPosition };
+}
