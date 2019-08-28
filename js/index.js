c654d2d646d483d6ebb6ac58fa1d98d8537e3dfd
Stephen Welch <stephenwelchva@gmail.com>
2019-08-27 22:51:29 -0400

    Start skeleton of new site

---
+++ js/index.js
@@ -0,0 +1,29 @@
+let blackOnWhite = {
+    foreground: "black",
+    background: "white"
+};
+
+let whiteOnBlack = {
+    foreground: "white",
+    background: "black"
+};
+
+let blackOnSepia = {
+    foreground: "black",
+    background: "#f7efd5" // sepia
+}
+
+let themes = [blackOnWhite, whiteOnBlack, blackOnSepia];
+
+let themeIndex = 0;
+
+function toggleColor() {
+    themeIndex++;
+    if(themeIndex >= themes.length) {
+        themeIndex = 0;
+    }
+    document.body.style.setProperty("--foreground-color", themes[themeIndex].foreground);
+    document.body.style.setProperty("--background-color", themes[themeIndex].background);
+
+    console.log("Toggling");
+}
